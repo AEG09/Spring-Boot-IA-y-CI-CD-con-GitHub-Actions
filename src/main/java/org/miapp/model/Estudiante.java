d36e8cefb9c97769713c7b1ee07eca45b6e6cff4
AEG09 <alvaro.eugenio.g@gmail.com>
2026-05-15 18:12:12 +0100

    feat: CRUD inicial de Spring Boot completado

---
+++ src/main/java/org/miapp/model/Estudiante.java
@@ -0,0 +1,26 @@
+package org.miapp.model;
+import jakarta.persistence.*;
+
+import lombok.Data; // Si usas Lombok, te ahorras escribir getters y setters
+
+
+
+@Entity
+
+@Table(name = "estudiantes")
+
+public class Estudiante {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String nombre;
+
+    private String email;
+
+    private int edad;
+
+
+}
